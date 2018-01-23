34c32036e0071894c63f526d889432fd685084fc
sonatasnirp <sonata.snirpunaite@gmail.com>
2018-01-23 13:46:16 +0200

    Delete searchform.php

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-	<fieldset>
-		<input type="text" name="s" value="" placeholder="<?php _e('To search type and hit enter...', 'az_alice'); ?>" />
-    </fieldset>
-</form>
